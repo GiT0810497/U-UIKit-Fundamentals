7a6c3720983f9de8d94cdddad847f2f6e1f4ae45
dhlsnow <dhlsnow@163.com>
2017-07-13 11:57:23 +0800

    Creating The Count And Label Properties

--- 1 Outlets and Actions/Outlets and Actions/Outlets and Actions/ViewController.swift
+++ 1 Outlets and Actions/Outlets and Actions/Outlets and Actions/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count = 10
+    var label:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,12 +21,19 @@ class ViewController: UIViewController {
         label.text = "0"
         self.view.addSubview(label)
         
+        self.label = label
+        
         var button = UIButton()
         button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60)
         button.setTitle("click", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
         self.view.addSubview(button)
         
+    }
+    
+    func incrementCount() {
+        self.count = self.count+1
+        self.label.text = "\(self.count)"
     }
 
 
