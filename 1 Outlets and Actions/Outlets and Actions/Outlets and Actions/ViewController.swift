dd11d700fa04e32261a02428150aedd840150e8a
dhlsnow <dhlsnow@163.com>
2017-07-13 13:14:33 +0800

    Setting The Target Action

--- 1 Outlets and Actions/Outlets and Actions/Outlets and Actions/ViewController.swift
+++ 1 Outlets and Actions/Outlets and Actions/Outlets and Actions/ViewController.swift
@@ -10,24 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var count = 10
-    var label:UILabel!
+    var count = 0
+    var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //label
         var label = UILabel()
         label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
         label.text = "0"
         self.view.addSubview(label)
-        
         self.label = label
         
+        //button
         var button = UIButton()
         button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60)
         button.setTitle("click", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
         self.view.addSubview(button)
+        button.addTarget(self, action: #selector(ViewController.incrementCount), for: .touchUpInside)
         
     }
     
