88dc82f6afed4178c44667351f1e49eeb0259f88
dhlsnow <dhlsnow@163.com>
2017-07-13 13:33:14 +0800

    add another label,button,toggle backgroundColor

--- 1 Outlets and Actions/Outlets and Actions/Outlets and Actions/ViewController.swift
+++ 1 Outlets and Actions/Outlets and Actions/Outlets and Actions/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     var count = 0
     var label: UILabel!
+    var label2 = UILabel()
+    var toggle = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,19 +25,51 @@ class ViewController: UIViewController {
         self.view.addSubview(label)
         self.label = label
         
+        label2.frame = CGRect.init(x: 150, y: 200, width: 60, height: 60)
+        label2.text = "0"
+        self.view.addSubview(label2)
+        
         //button
         var button = UIButton()
         button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60)
-        button.setTitle("click", for: .normal)
+        button.setTitle("increase", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
         self.view.addSubview(button)
         button.addTarget(self, action: #selector(ViewController.incrementCount), for: .touchUpInside)
+        
+        var decrementButton = UIButton()
+        decrementButton.frame = CGRect.init(x: 150, y: 300, width: 60, height: 60)
+        decrementButton.setTitle("decrement", for: .normal)
+        decrementButton.setTitleColor(UIColor.blue, for: .normal)
+        self.view.addSubview(decrementButton)
+        decrementButton.addTarget(self, action: #selector(ViewController.decrementCount), for: .touchUpInside)
         
     }
     
     func incrementCount() {
         self.count = self.count+1
         self.label.text = "\(self.count)"
+        self.label2.text = "\(self.count)"
+        togglesBackground()
+    }
+    
+    func decrementCount() {
+        self.count = self.count-1
+        self.label.text = "\(self.count)"
+        self.label2.text = "\(self.count)"
+        togglesBackground()
+    }
+
+    
+    func togglesBackground() {
+        var color = UIColor()
+        if toggle{
+            color = UIColor.gray
+        }else{
+            color = UIColor.white
+        }
+        self.view.backgroundColor = color
+        toggle = !toggle
     }
 
 
